a681649f6752521e642ed5dcc29bd9a467805b93
Yaxin Yang <yyang66@slb.com>
2023-11-02 13:43:09 +0800

    feat: change the name and speed to property

---
+++ OOPractice/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOPractice
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+
+        public string SpeedUp()
+        {
+            return $"{Name}:speed up {Speed} km/h";
+        }
+    }
+}
--- OOPracticeTest/UnitTest1.cs
+++ OOPracticeTest/UnitTest1.cs
@@ -1,15 +1,19 @@
+using OOPractice;
+using Xunit;
 namespace OOPracticeTest
 {
-    using OOPractice;
-    using Xunit;
-
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+        public void Shoule_return_name_and_speed_When_speedup_Given_nameandspeed()
+        {//GIVEN
+            Car car = new Car(); //构造方法
+            car.Name = "Big truck";
+            car.Speed = 10;
+            //WHEN
+            string result = car.SpeedUp();
+            //THEN
+            Assert.Equal("Big truck:speed up 10 km/h", result);
         }
     }
 }
